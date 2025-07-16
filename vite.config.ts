94f8ffc9f53db800c74b6687bcf592a7b1584f18
Emmanuel Gama <emmanuel.gama.ibarra@gmail.com>
2025-07-15 22:51:43 -0600

    Configure Vite server to use PORT environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const PORT = parseInt(process.env.PORT || '8080', 10);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: PORT,
+    host: '0.0.0.0',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
